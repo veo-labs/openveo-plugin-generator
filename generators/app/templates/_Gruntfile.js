fde75731b9d027bc41b7a2910ee5164882380d29
Maxime Béguin <maxime.beguin@veo-labs.com>
2021-10-21 15:39:16 +0200

    dependencies: remove plugins grunt-karma
    grunt-karma was used to launch generated plugins
    client unit tests.
    karma is now launched directly using npx,
    consequently grunt-karma is no longer needed and
    has been removed.

--- generators/app/templates/_Gruntfile.js
+++ generators/app/templates/_Gruntfile.js
@@ -42,7 +42,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mkdocs');
 
